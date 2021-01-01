585a083ec6c68052e58aaae084910afdc8578249
S-Konokata <76766158+S-konokata@users.noreply.github.com>
2021-01-01 02:34:34 +0000

    Add: HTML class of index.html for bootstrap.

--- metar_server/metarapp/forms.py
+++ metar_server/metarapp/forms.py
@@ -2,10 +2,15 @@ from django import forms
 
 
 class MetarAppForm(forms.Form):
-    icao = forms.CharField(label='空港コード（RJ__）')
+    icao = forms.CharField(
+        label='空港コード（RJ__）',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'}
+        )
+    )
     search_date = forms.DateField(
         label='日付',
         widget=forms.DateInput(
-            attrs={'class': 'flatpickr'}
+            attrs={'class': 'form-control flatpickr'}
         )
     )
