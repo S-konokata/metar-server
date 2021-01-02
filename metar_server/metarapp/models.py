a96914f2b0fd8d22dd21d044528a56e36ba254a2
bais_khi <connie_gmnt@live.jp>
2021-01-02 02:54:38 +0000

    Add: Create Metar model.

--- metar_server/metarapp/models.py
+++ metar_server/metarapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Metar(models.Model):
+    # "calc" comment column is needed to be converted from raw text. 
+    raw_text = models.TextField()
+    station_id = models.CharField(max_length=4)
+    observation_time = models.DateTimeField()
+    temp_c = models.FloatField()
+    dewpoint_c = models.FloatField()
+    wind_dir_degrees = models.PositiveSmallIntegerField()
+    wind_speed_kt = models.PositiveSmallIntegerField()
+    wind_gust_kt = models.PositiveSmallIntegerField()
+    visibility_m = models.PositiveSmallIntegerField()  # calc
+    altim_in_hg = models.FloatField()  # calc
+    wx_string = models.CharField(max_length=12)
+    cloud_ceiling = models.PositiveIntegerField()  # calc(from other column)
+    metar_type = models.CharField(max_length=5)
