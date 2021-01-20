e3db37da147803a1a807793f0137593ada007e8c
bais_khi <connie_gmnt@live.jp>
2021-01-20 04:51:16 +0000

    Add: get_airport_list to metar_function.py

--- metar_server/metarapp/management/commands/fetchMetar.py
+++ metar_server/metarapp/management/commands/fetchMetar.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from typing import Any, Optional
-from metar_server.metarapp.models import Airport, Metar
 from metar_server.metarapp.myfunction import metar_function
 
 
@@ -8,13 +7,14 @@ class Command(BaseCommand):
     help = 'Fetch METAR data and save to database.'
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
-        airport_list_query = Airport.objects \
-            .filter(
-                is_fetched=True
-            ) \
-            .values_list('station_id')
-        airport_list = list(airport_list_query)
+        """Command of fetching and saving METARs.
+
+        Returns:
+            Optional[str]: Options for inherited function.
+        """
+        airport_list = metar_function.get_airport_list()
         metar_input = metar_function.MetarInput(airport_list)
+        metar_input.fetch_and_save()
         self.stdout.write('Fetched airport is %s' % airport_list)
         self.stdout.write('Fetched time is %s' % metar_input.fetched_time)
         return super().handle(*args, **options)
