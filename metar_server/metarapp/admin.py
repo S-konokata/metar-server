536b9a87b3cd1910b470f3c386623d92a1953eae
bais_khi <connie_gmnt@live.jp>
2021-01-30 09:09:34 +0000

    Add: Create metar manage page.

--- metar_server/metarapp/admin.py
+++ metar_server/metarapp/admin.py
@@ -1,5 +1,54 @@
+from __future__ import annotations
 from django.contrib import admin
-from .models import Metar
+from django.shortcuts import render
+from django.urls import path, URLPattern
+from .forms import GetMetarNowForm
+from .models import Airport, Metar
+from metar_server.metarapp.myfunction import airport_function, metar_function
 
 
-admin.site.register(Metar)
+@admin.register(Metar)
+class MetarAdmin(admin.ModelAdmin):
+    def get_urls(self) -> list[URLPattern]:
+        urls = super().get_urls()
+        my_urls = [
+            path(
+                'metar_manage_view',
+                self.admin_site.admin_view(self.metar_manage_view()),
+                name='metar_manage'
+            )
+        ]
+        return my_urls + urls
+
+    def metar_manage_view(self, request):
+        airport_metar_newest = airport_function.AirportMetarNewest()
+        params = {
+            'newest_datetimes': airport_metar_newest.newest_dict,
+            'form': GetMetarNowForm(airport_metar_newest.airport_list),
+            'get_metar_response': ''
+        }
+        if (request.method == 'POST'):
+            fetch_airport: list[str] = request.POST.getlist('airport')
+            get_metar = metar_function.MetarInput(fetch_airport)
+            get_metar.fetch_and_save()
+            metar_num_dict: dict[str: int] = {}
+            for metar in get_metar.fetched_data:
+                if metar['station_id'] in metar:
+                    metar_num_dict[metar['station_id']] += 1
+                else:
+                    metar_num_dict[metar['station_id']] = 1
+            metar_all_num = 0
+            for station in metar_num_dict:
+                metar_all_num += station
+            fetched_time = get_metar.fetched_time.strftime(r"%Y %b %d %X %Z")
+            params['get_metar_response'] = [
+                'The METARs are fetched.',
+                'The number of the METAR data in each airport:',
+                str(metar_num_dict),
+                f'The total number: {metar_all_num}',
+                f'fetched datetime: {fetched_time}'
+            ]
+        return render(request, 'admin/metarapp/metar/metar_manage_view.html', params)
+
+
+admin.site.register(Airport)
--- metar_server/metarapp/forms.py
+++ metar_server/metarapp/forms.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+from typing import Any, Mapping, Optional, Type, Union
 from django import forms
+from django.forms.utils import ErrorList
 
 
 class MetarAppForm(forms.Form):
@@ -27,3 +29,29 @@ class MetarAppForm(forms.Form):
 
     def clean(self) -> dict[str, any]:
         return super().clean()
+
+
+class GetMetarNowForm(forms.Form):
+    airport_choices = []
+    airport = forms.MultipleChoiceField(
+        label='Choose airport',
+        choices=airport_choices,
+        widget=forms.SelectMultiple(attrs={'size': 5, 'id': 'airport_select'})
+    )
+
+    def __init__(self, airport_list: list[str], data: Optional[Mapping[str, Any]],
+                 files: Optional[Mapping[str, Any]], auto_id: Optional[Union[bool, str]], prefix: Optional[str],
+                 initial: Optional[Mapping[str, Any]], error_class: Type[ErrorList], label_suffix: Optional[str],
+                 empty_permitted: bool, field_order: Optional[Any], use_required_attribute: Optional[bool],
+                 renderer: Any) -> None:
+        """Add airports to choices of this form.
+
+        Other arguments are for super().__init__()
+
+        Args:
+            airport_list (list[str]): airport list from Airport model.
+        """
+        self.airport_choices.extend(airport_list)
+        super().__init__(data=data, files=files, auto_id=auto_id, prefix=prefix, initial=initial,
+                         error_class=error_class, label_suffix=label_suffix, empty_permitted=empty_permitted,
+                         field_order=field_order, use_required_attribute=use_required_attribute, renderer=renderer)
