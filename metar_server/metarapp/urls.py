6314fdf73af7e2ee296febba6aa2df74dbfda8fb
bais_khi <connie_gmnt@live.jp>
2021-03-06 09:12:38 +0000

    Chg: Navbar, and login redirect URL

--- metar_server/metarapp/urls.py
+++ metar_server/metarapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'metarapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.Login.as_view(), name='login'),
